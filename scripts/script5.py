cdb85ae1a13299f2c62dc55c621102a2bd15a763
NuwanJ <nuwanjaliyagoda@gmail.com>
2020-10-18 13:36:18 +0530

    coordination system updated

--- scripts/script5.py
+++ scripts/script5.py
@@ -4,11 +4,18 @@ import paho.mqtt.client as paho
 import json
 import math
 import time
+import threading
 
+camera_id = 1
+
+# -- Coordinate system variables -----------------------------------------------
 robots = {}
 update_xy_threshold = 10       # units
 update_heading_threshold = 10    # degrees
+x_scale = 1.0
+y_scale = 1.0
 
+# -- MQTT variables ------------------------------------------------------------
 mqtt_server="68.183.188.135"
 mqtt_port=1883
 mqtt_keepalive=60
@@ -19,122 +26,125 @@ sub_topic_publish="v1/localization/info"
 sub_topic_update_robot="v1/localization/update/robot"
 sub_topic_create="v1/gui/create"
 
+# -- MQTT loop thread function - NOT WORKING FOR NOW ---------------------------
+def mqtt_loop(client):
+    client.loop()
+
+def mqtt_setup():
+    client = paho.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(mqtt_server, mqtt_port, mqtt_keepalive)
+    time.sleep(2)
+    client.loop()
+    return client
+
 def update_robot(id, x, y, heading):
-    #print([id, x, y, heading])
     id = int(id)
 
     if id in robots:
         old = robots[id];
-        if( (math.sqrt(abs( pow(x-old['x'], 2) + pow(y-old['y'], 2))) >= update_xy_threshold)):
-
-            print('Robot coordinate update received')
-            #  or (abs(old['heading'] - heading) >= update_heading_threshold)
-            # update the server about new coordinates
-            robots[id]['x'] = x
-            robots[id]['y'] = y
+        if( (math.sqrt(abs( pow(x-old['x'], 2) + pow(y-old['y'], 2))) >= update_xy_threshold)): #  or (abs(old['heading'] - heading) >= update_heading_threshold)
+            # update the server about new coordinates, if there is any significant difference
+            robots[id]['x'] = x*x_scale
+            robots[id]['y'] = y*y_scale
             robots[id]['heading'] = heading
+            print(["update", id, x, y, heading])
             client.publish(sub_topic_publish, json.dumps(robots[id]), qos=1)
+            client.loop()
     else:
         # create and publish
         robots[id] =  {'id':id, 'x':x, 'y':y, 'heading':heading}
         client.publish(sub_topic_publish, json.dumps(robots[id]), qos=1)
+        client.loop()
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print("Connected to MQTT server - "+str(rc))
     client.subscribe(sub_topic_update)
     client.subscribe(sub_topic_create)
     client.subscribe(sub_topic_update_robot)
-
     # Adding a robot into the data  structure - only for debug
-    robots[0] = {'id':0, 'x':0.0, 'y':0.0, 'heading':0.0}
-
+    #robots[0] = {'id':0, 'x':0.0, 'y':0.0, 'heading':0.0}
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" > "+str(msg.payload, 'utf-8'))
-
+    #print(msg.topic+" > "+str(msg.payload, 'utf-8'))
     topic = msg.topic
     body = str(msg.payload, 'utf-8')
 
     if (topic==sub_topic_update):
-        #print((robots))
-        client.publish(sub_topic_publish, json.dumps(robots, indent=2), qos=1)
+        # Update the coordinates of all active robots
+        client.publish(sub_topic_publish, json.dumps(robots,sort_keys=True), qos=1)
 
-    elif (topic== sub_topic_create):    # only for testings purposes
+    elif (topic== sub_topic_create):
+        # create a robot - only for testings purposes
         d=json.loads(body)
         robots[d['id']] = d
-        print(robots)
+
     elif (topic == sub_topic_update_robot):
+        # manually call update function - only for testing purposes
         d=json.loads(body)
-        #print(d)
         update_robot(d['id'], d['x'], d['y'], d['heading'])
 
 
-# -- MQTT Client ---------------------------------------------------------------
+# -- OpenCV Image processing ---------------------------------------------------
 
 # Load the predefined dictionary
 dictionary = cv.aruco.Dictionary_get(cv.aruco.DICT_6X6_250)
 dictionary = cv.aruco.Dictionary_get(cv.aruco.DICT_6X6_250)
 parameters = cv.aruco.DetectorParameters_create()
 
-# Load camera calibrations ---------------------------------------------------
+# -- Load camera calibrations --------------------------------------------------
 cv_file = cv.FileStorage('../board/calibration_data.txt', cv.FILE_STORAGE_READ)
 cameraMatrix = cv_file.getNode("K").mat()
 distCoeffs = cv_file.getNode("D").mat()
 cv_file.release()
 
-print(cameraMatrix)
-print(distCoeffs)
+#print(cameraMatrix)
+#print(distCoeffs)
 
-# ----------------------------------------------------------------------------
+
+# ******************************************************************************
+# ******************************************************************************
 
 if __name__ == '__main__':
 
-    client = paho.Client()
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect(mqtt_server, mqtt_port, mqtt_keepalive)
-    client.loop()
-    time.sleep(2)
+    # Starts MQTT client
+    client = mqtt_setup()
+
+    '''
+    try:
+        t1 = threading.Thread(target=mqtt_loop, args=(client,))
+        t1.start()
+    except:
+       print("Error: unable to start thread")
+    '''
 
     print('Press "q" to quit')
-    capture = cv.VideoCapture(1)
 
+    capture = cv.VideoCapture(camera_id)
     if capture.isOpened():  # try to get the first frame
         frame_captured, frame = capture.read()
     else:
         frame_captured = False
 
-    counter = 0;
-
     while frame_captured:
 
-        if counter>=5:
-            counter = 0
-            client.loop()
-        else:
-            counter=counter+1
-
-        # detectMarkers(inputImage, dictionary, markerCorners, markerIds, parameters, rejectedCandidates);
         markerCorners, markerIds, rejectedCandidates = cv.aruco.detectMarkers(frame, dictionary, parameters=parameters)
 
         # Non-empty array of markers
         if (type(markerIds) != type(None)):
-
-            # drawDetectedMarkers(outputImage, markerCorners, markerIds)
             cv.aruco.drawDetectedMarkers(frame, markerCorners, markerIds);
 
             # estimatePoseSingleMarkers(markerCorners, size_of_marker_in_real, cameraMatrix, distCoeffs, rvecs, tvecs);
             rvecs, tvecs, _objPoints = cv.aruco.estimatePoseSingleMarkers(markerCorners, 50, cameraMatrix, distCoeffs)
 
             for i in range(len(markerIds)):
-
                 id = markerIds[i][0]
-
                 coordinate = tvecs[i][0]
 
-                x = math.floor(coordinate[0])
-                y = math.floor(coordinate[1])
-                rotation = math.floor((rvecs[i][0][1]/math.pi)*180.0)
+                x = math.floor(coordinate[0])                           # center = 0
+                y = math.floor(coordinate[1])                           # center = 0
+                heading = math.floor((rvecs[i][0][1]/math.pi)*180.0)    # [-180, 180]
                 #print(id, x, y, rotation ) # rvecs[i],
                 update_robot(id, x, y, 0)
 
